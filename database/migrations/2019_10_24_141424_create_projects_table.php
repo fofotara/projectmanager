c51130d0641bbbbcf86d4da5aff68a4de537c414
bitenta <bitenta@DESKTOP-9CPDETN>
2019-10-25 17:03:26 +0300

    Nested List

---
+++ database/migrations/2019_10_24_141424_create_projects_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->text('address')->nullable();
+            $table->string('image')->default('default-home.jpg');
+            $table->string('startDate')->nullable();
+            $table->string('endDate')->nullable();
+            $table->text('description')->nullable();
+            $table->decimal('budget')->default(0);
+            $table->decimal('area')->default(0);
+            $table->integer('floor')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
