f64692aea578616bb047747aa261292eb47ba0da
bitenta <bitenta@DESKTOP-9CPDETN>
2019-10-31 13:15:54 +0300

    User İşlemleri

--- resources/views/usersView/index.blade.php
+++ resources/views/usersView/index.blade.php
@@ -15,44 +15,45 @@
                 </div>
             </div>
             <div class="block-content">
-                <table class="table table-hover table-center">
-                    <thead>
-                    <tr>
-                        <th class="text-center" style="width: 50px;">#</th>
-                        <th>Adı Soyadı</th>
-                        <th class="d-none d-sm-table-cell" style="width: 15%;">Role</th>
-                        <th class="text-center" style="width: 100px;">--</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @foreach($users as $key => $user)
+                <div class="table-responsive"> <table class="table table-hover table-center">
+                        <thead>
                         <tr>
-                            <th class="text-center" scope="row">{{$key+1}}</th>
-                            <td>{{$user->name}}</td>
-                            <td class="d-none d-sm-table-cell">
-                                @if(!empty($user->getRoleNames()))
-                                    @foreach($user->getRoleNames() as $v)
-                                        <label class="badge badge-dark">{{ $v }}</label>
-                                    @endforeach
-                                @endif
-                            </td>
-                            <td class="text-center">
-                                <div class="btn-group">
-                                    <button type="button" class="btn btn-sm btn-warning" data-toggle="tooltip"
-                                            title="Edit">
-                                        <i class="fa fa-pencil"></i>
-                                    </button>
-                                    <button type="button" class="btn btn-sm btn-danger" data-toggle="tooltip"
-                                            title="Delete">
-                                        <i class="fa fa-times"></i>
-                                    </button>
-                                </div>
-                            </td>
+                            <th class="text-center" style="width: 50px;">#</th>
+                            <th>Adı Soyadı</th>
+                            <th class="d-none d-sm-table-cell" style="width: 15%;">Role</th>
+                            <th class="text-center" style="width: 100px;">--</th>
                         </tr>
-                    @endforeach
+                        </thead>
+                        <tbody>
+                        @foreach($users as $key => $user)
+                            <tr>
+                                <th class="text-center" scope="row">{{$key+1}}</th>
+                                <td>{{$user->name}}</td>
+                                <td class="d-none d-sm-table-cell">
+                                    @if(!empty($user->getRoleNames()))
+                                        @foreach($user->getRoleNames() as $v)
+                                            <label class="badge badge-dark">{{ $v }}</label>
+                                        @endforeach
+                                    @endif
+                                </td>
+                                <td class="text-center">
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-sm btn-warning" data-toggle="tooltip"
+                                                title="Edit">
+                                            <i class="fa fa-pencil"></i>
+                                        </button>
+                                        <button type="button" class="btn btn-sm btn-danger" data-toggle="tooltip"
+                                                title="Delete">
+                                            <i class="fa fa-times"></i>
+                                        </button>
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
 
-                    </tbody>
-                </table>
+                        </tbody>
+                    </table></div>
+
             </div>
         </div>
 
